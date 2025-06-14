e4cbbc62a0edf55e2d9625e8f8af73f5d8c54b31
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-06-14 08:36:58 +0200

    Update model_name

--- chesstune/config.py
+++ chesstune/config.py
@@ -6,5 +6,5 @@ from pydantic import BaseModel
 class Config(BaseModel):
     """Configuration for the application."""
 
-    model_name: str = 'Qwen/Qwen3-4B-GGUF'
+    model_name: str = 'Qwen/Qwen3-1.7B'
     data_dir: Path = Path('data/pgn')
